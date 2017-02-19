e8e80e5b19cad0c8f9bef9d7614f4e30c97553f6
Fenirr <162t0305@itsm.edu.mx>
2017-02-18 23:22:28 -0600

    sin retorno ,con parametros
    ejemplo de metodos en java

---
+++ metodo2ejemplo1.java
@@ -0,0 +1,15 @@
+package metodo2;
+
+public class ejemplo1 {
+public static void main(String[]args){
+
+	multiplicar(5, 9);
+}
+	public static void multiplicar(int b,int d){
+		int multiplicar= b*d;
+		System.out.println("el cociente es:"+multiplicar);
+	}
+	
+	
+}
+
